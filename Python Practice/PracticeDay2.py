cc73b3a47c943c8b95b79d4e9b44da147ca34de5
donjo352 <jid@princeton.edu>
2017-06-14 15:37:43 -0400

    sending to DLL

--- Python Practice/PracticeDay2.py
+++ Python Practice/PracticeDay2.py
@@ -47,3 +47,16 @@ import numpy as np
 # print(boo)
 
 #Problem 4
+ import matplotlib.pyplot as plt
+
+ x = np.arange(0.,10.,0.1)
+ y = np.arange(0.,10.,0.1)
+ x,y = np.meshgrid(x,y)
+ mean_xy = np.array([5.,4.])
+ cov = np.array([[1.,1.],
+                 [1.,2.]])**2.
+
+ f = gaussian2D(x.ravel(), y.ravel(), mean_xy, cov)
+ f.reshape(100,100)
+
+ plt.imshow(f.reshape(100,100))
